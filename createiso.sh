a901f3c6e355e41cc97099f680aef3df42c027b9
Patrick Callahan <pmc@patrickcallahan.com>
2014-08-21 23:47:59 -0400

    Minor cleanup.
    * Fix typos in README
    * Wrap variables in quotes. Script failed if path to ISO contained spaces.
    * Implement some suggestions from Google shell style guide.
    * Address some shellcheck findings.
    * Add a debug mode.
    * Remove unused options.
    * Move some repeated code to a function.

--- createiso.sh
+++ createiso.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 ###############################################################################
 # STIG FIX DVD CREATOR
 #
@@ -9,18 +9,22 @@
 #
 # Author: Frank Caviggia (fcaviggi@redhat.com)
 # Copyright: Red Hat, (c) 2013
-# Version: 1.1.3
+# Version: 1.1.4
 # License: GPLv2
 # Description: Kickstart Installation of RHEL 6 with DISA STIG 
 ###############################################################################
 
 # GLOBAL VARIABLES
-DIR=`pwd`
+readonly DIR=$(pwd)
+readonly MOUNT_POINT="/rhel"
 
 # USAGE STATEMENT
-function usage() {
+usage() {
 cat << EOF
-usage: $0 rhel-server-6.5-x86_64-dvd.iso
+usage: ${0##*/} [-hd] rhel-server-6.5-x86_64-dvd.iso
+
+  -h  show help and exit
+  -d  debug
 
 DISA STIG Installer Kickstart RHEL 6.4+
 
@@ -31,23 +35,35 @@ with the following hardening:
   - DISA STIG for Firefox (User/Developer Workstation)
   - Classification Banner (Graphical Desktop)
 
+This tool must be run as root.
+
 EOF
 }
 
-while getopts ":vhq" OPTION; do
-	case $OPTION in
-		h)
-			usage
-			exit 0
-			;;
-		?)
-			echo "ERROR: Invalid Option Provided!"
-			echo
-			usage
-			exit 1
-			;;
-	esac
+cleanup() {
+    umount $MOUNT_POINT
+    rm -rf $MOUNT_POINT
+}
+
+OPTIND=1
+while getopts "dh" OPTION; do
+    case $OPTION in
+        d)
+            set -x
+            ;;
+        h)
+            usage
+            exit 0
+            ;;
+        *)
+            echo "ERROR: Invalid Option Provided!"
+            echo
+            usage
+            exit 1
+            ;;
+    esac
 done
+shift "$((OPTIND-1))" # Shift off the options and optional --.
 
 # Check for root user
 if [[ $EUID -ne 0 ]]; then
@@ -59,52 +75,49 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # Determine if DVD is Bootable
-`file $1 | grep 9660 | grep -q bootable`
+file "$1" | grep 9660 | grep -q bootable
 if [[ $? -eq 0 ]]; then
 	echo "Mounting RHEL DVD Image..."
-	mkdir -p /rhel
-	mkdir $DIR/rhel-dvd
-	mount -o loop $1 /rhel
+	mkdir -p $MOUNT_POINT
+	mkdir "$DIR"/rhel-dvd
+	mount -o loop "$1" $MOUNT_POINT
 	echo "Done."
 	# Tests DVD for RHEL 6.4+
-	if [[ $(grep "Red Hat" /rhel/.discinfo | awk '{ print $5 }' | awk -F '.' '{ print $1 }') -ne 6 ]]; then
+	if [[ $(grep "Red Hat" $MOUNT_POINT/.discinfo | awk '{ print $5 }' | awk -F '.' '{ print $1 }') -ne 6 ]]; then
 		echo "ERROR: Image is not RHEL 6.4+"
-		umount /rhel
-		rm -rf /rhel
+        cleanup
 		exit 1
 	fi
-	if [[ $(grep "Red Hat" /rhel/.discinfo | awk '{ print $5 }' | awk -F '.' '{ print $2 }') -lt 4 ]]; then
+	if [[ $(grep "Red Hat" $MOUNT_POINT/.discinfo | awk '{ print $5 }' | awk -F '.' '{ print $2 }') -lt 4 ]]; then
 		echo "ERROR: Image is not RHEL 6.4+"
-		umount /rhel
-		rm -rf /rhel
+        cleanup
 		exit 1
 	fi
 	echo -n "Copying RHEL DVD Image..."
-	cp -a /rhel/* $DIR/rhel-dvd/
-	cp -a /rhel/.discinfo $DIR/rhel-dvd/
+	cp -a $MOUNT_POINT/* "$DIR/rhel-dvd/"
+	cp -a $MOUNT_POINT/.discinfo "$DIR/rhel-dvd/"
 	echo " Done."
-	umount /rhel
-	rm -rf /rhel
+    cleanup
 else
 	echo "ERROR: ISO image is not bootable."
 	exit 1
 fi
 
 echo -n "Modifying RHEL DVD Image..."
-cp -a $DIR/config/* $DIR/rhel-dvd/
+cp -a "$DIR/config/*" "$DIR/rhel-dvd/"
 echo " Done."
 
 echo "Remastering RHEL DVD Image..."
-cd $DIR/rhel-dvd
+cd "$DIR/rhel-dvd"
 chmod a+w isolinux/isolinux.bin
 find . -name TRANS.TBL -exec rm '{}' \; 
-/usr/bin/mkisofs -J -T -o $DIR/rhel-stig-fix.iso -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -R -m TRANS.TBL .
-cd $DIR
-rm -rf $DIR/rhel-dvd
+/usr/bin/mkisofs -J -T -o "$DIR/rhel-stig-fix.iso" -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -R -m TRANS.TBL .
+cd "$DIR"
+rm -rf "$DIR/rhel-dvd"
 echo "Done."
 
 echo "Signing RHEL DVD Image..."
-/usr/bin/implantisomd5 $DIR/rhel-stig-fix.iso
+/usr/bin/implantisomd5 "$DIR/rhel-stig-fix.iso"
 echo "Done."
 
 echo "DVD Created. [rhel-stig-fix.iso]"
