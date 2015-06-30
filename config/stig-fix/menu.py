28c7591c997e7cc3ed72f20436c134e25e54930a
jgr208 <jricles@jasonricles-PC.mikrossystems.com>
2015-06-30 14:09:30 -0400

    Revert "added tcpdump"
    This reverts commit ef8a174f83e59c82befe9bfba9fe4fc879fdade6.

--- config/stig-fix/menu.py
+++ config/stig-fix/menu.py
@@ -412,7 +412,6 @@ class Display_Menu:
 		f.write('dialog\n')
 		f.write('dmidecode\n')
 		f.write('aide\n')
-		f.write('tcpdump\n')
 		f.close()
 
 
@@ -480,7 +479,6 @@ class Display_Menu:
 			f.write('dialog\n')
 			f.write('dmidecode\n')
 			f.write('aide\n')
-			f.write('tcpdump\n')
 			f.close()
 
 		################################################################################################################
@@ -532,7 +530,6 @@ class Display_Menu:
 			f.write('gcc-c++\n')
 			f.write('dos2unix\n')
 			f.write('kernel-devel\n')
-			f.write('tcpdump\n')
 			f.write('gcc\n')
 			f.write('dialog\n')
 			f.write('dmidecode\n')
